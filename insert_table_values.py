bb4254120196202f49650980a06cca312de4cf1e
Osman Mohammed <os.mohammed.osman@gmail.com>
2020-03-21 10:35:30 +0100

    values inserted to file

--- insert_table_values.py
+++ insert_table_values.py
@@ -3,7 +3,7 @@
 
 import connect_db
 # Importer le fichier "InsertOneTable" dans lequel il y a quelques classes et méthodes en rapport avec le sujet d'insertion dans UNE SEULE table.
-import insert_one_table
+from INSERT import insert_one_table
 
 # OM 202.03.11 Démonstration en classe, juste pour faire un objet "jeveux_etre_connecte"
 
@@ -12,11 +12,11 @@ objet_etre_connecte = connect_db.DatabaseTools()
 # OM 2020.01.28 Une instance "insert_records" pour permettre l'utilisation des méthodes de la classe DbInsertOneTable
 insert_records = insert_one_table.DbInsertOneTable()
 
-valeur_debile_mais_presque_aleatoire_a_inserer = "bonjou5re5r"
+valeur_debile_mais_presque_aleatoire_a_inserer = "adem"
 print(valeur_debile_mais_presque_aleatoire_a_inserer)
 
-
-insert_records.insert_one_record_one_table("INSERT IGNORE INTO t_genres (id_genre, intitule_genre) VALUES (null, %(values_insert)s)",valeur_debile_mais_presque_aleatoire_a_inserer)
-
+insert_records.insert_one_record_one_table(
+    "INSERT IGNORE INTO t_user (id_user, Nom) VALUES (null, %(values_insert)s)",
+    valeur_debile_mais_presque_aleatoire_a_inserer)
 
 objet_etre_connecte.close_connection()
